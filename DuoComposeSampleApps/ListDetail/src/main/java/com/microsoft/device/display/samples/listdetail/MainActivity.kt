118e633058cdd8940a242313555d24f1e7baa045
Joy Liu <joyl1216@hotmail.com>
2021-01-12 13:54:12 -0800

    Introduce DuoComposeSampleApps

---
+++ DuoComposeSampleApps/ListDetail/src/main/java/com/microsoft/device/display/samples/listdetail/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.microsoft.device.display.samples.listdetail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
+import com.microsoft.device.display.samples.listdetail.ui.DuoComposeSampleAppsTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DuoComposeSampleAppsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    BasicText(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DuoComposeSampleAppsTheme {
+        Greeting("Android")
+    }
+}
