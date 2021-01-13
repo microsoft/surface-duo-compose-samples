80c1843ef5ff58e3886597efaccf75c37259b56f
Joy Liu <joyl1216@hotmail.com>
2021-01-13 11:21:09 -0800

    Bring in Compose Sample

---
+++ ComposeSample/app/src/main/java/com/microsoft/device/display/samples/composesample/ui/Shape.kt
@@ -0,0 +1,18 @@
+/*
+ *
+ *  * Copyright (c) Microsoft Corporation. All rights reserved.
+ *  * Licensed under the MIT License.
+ *
+ */
+
+package com.microsoft.device.display.samples.composesample.ui
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
