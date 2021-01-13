ab1786ef3aaa8fabf49363909406ae5f622e4c02
Joy Liu <joyl1216@hotmail.com>
2021-01-12 16:43:10 -0800

    Introduce CompanionPane sample

---
+++ CompanionPane/app/src/main/java/com/microsoft/device/display/samples/companionpane/ui/Color.kt
@@ -0,0 +1,11 @@
+package com.microsoft.device.display.samples.companionpane.ui
+
+import androidx.compose.ui.graphics.Color
+
+val purple200 = Color(0xFFBB86FC)
+val purple500 = Color(0xFF6200EE)
+val purple700 = Color(0xFF3700B3)
+val teal200 = Color(0xFF03DAC5)
+val darkGray = Color(0xFF2B2B2B)
+val Gray = Color(0xFF555555)
+val blueTint = Color(0xFF2BA1FF)
