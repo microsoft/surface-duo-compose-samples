63e6decc0b8dd3cf75d213c32b0dff893fcdf388
Joy Liu <joyliu1@microsoft.com>
2021-02-24 13:52:39 -0800

    Combine ListDetail and TwoPage due to same configuration

---
+++ TwoPage/TwoPage/src/main/java/com/microsoft/device/display/samples/twopage/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.microsoft.device.display.samples.twopage
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.microsoft.device.display.samples.twopage.ui.theme.TwoPageTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TwoPageTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    val openDialog = remember { mutableStateOf(true) }
+    val dialogWidth = 200.dp
+    val dialogHeight = 50.dp
+
+    Text(text = "Hello $name!")
+
+    if (openDialog.value) {
+        Dialog(onDismissRequest = { openDialog.value = false }) {
+            // Draw a rectangle shape with rounded corners inside the dialog
+            Box(Modifier.size(dialogWidth, dialogHeight).background(Color.White)){
+                Text(text = "Testing")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TwoPageTheme {
+        Greeting("Android")
+    }
+}
