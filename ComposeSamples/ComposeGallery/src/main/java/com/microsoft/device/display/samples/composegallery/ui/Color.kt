bfce2024dc1566a817b2f284a596a71ebe035eb1
Joy Liu <joyliu1@microsoft.com>
2021-02-25 17:11:27 -0800

    Add ComposeGallery

---
+++ ComposeSamples/ComposeGallery/src/main/java/com/microsoft/device/display/samples/composegallery/ui/Color.kt
@@ -0,0 +1,15 @@
+/*
+ *
+ *  * Copyright (c) Microsoft Corporation. All rights reserved.
+ *  * Licensed under the MIT License.
+ *
+ */
+
+package com.microsoft.device.display.samples.composegallery.ui
+
+import androidx.compose.ui.graphics.Color
+
+val purple200 = Color(0xFFBB86FC)
+val purple500 = Color(0xFF6200EE)
+val purple700 = Color(0xFF3700B3)
+val teal200 = Color(0xFF03DAC5)
