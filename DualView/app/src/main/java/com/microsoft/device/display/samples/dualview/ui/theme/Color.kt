5f940cd901b712b7ff18f223c1aaa68bf10be907
Joy Liu <joyliu1@microsoft.com>
2021-02-26 11:32:37 -0800

    Remove the duplicate projects

--- DualView/app/src/main/java/com/microsoft/device/display/samples/dualview/ui/theme/Color.kt
+++
@@ -1,7 +0,0 @@
-package com.microsoft.device.display.samples.dualview.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val gray500 = Color(0xFF9E9E9E)
-val red200 = Color(0xFFF54D4D)
-val red500 = Color(0xFFF65F5F)
