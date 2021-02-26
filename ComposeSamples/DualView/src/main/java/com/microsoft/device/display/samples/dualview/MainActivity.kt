dee5d6e03f3a9c858b13365195ff2e8e6d02457d
Joy Liu <joyliu1@microsoft.com>
2021-02-25 16:40:23 -0800

    Add DualView

---
+++ ComposeSamples/DualView/src/main/java/com/microsoft/device/display/samples/dualview/MainActivity.kt
@@ -0,0 +1,69 @@
+/*
+ *
+ *  * Copyright (c) Microsoft Corporation. All rights reserved.
+ *  * Licensed under the MIT License.
+ *  *
+ *
+ */
+
+package com.microsoft.device.display.samples.dualview
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.window.WindowManager
+import com.microsoft.device.display.samples.dualview.models.AppStateViewModel
+import com.microsoft.device.display.samples.dualview.ui.theme.DualViewComposeSampleTheme
+import java.util.concurrent.Executor
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var windowManager: WindowManager
+    private lateinit var appStateViewModel: AppStateViewModel
+
+    private val handler = Handler(Looper.getMainLooper())
+    private val mainThreadExecutor = Executor { r: Runnable -> handler.post(r) }
+
+    private val initialSelection = -1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        windowManager = WindowManager(this)
+        appStateViewModel = ViewModelProvider(this).get(AppStateViewModel::class.java)
+        appStateViewModel.setSelectionLiveData(initialSelection)
+        val isPortrait = resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+        appStateViewModel.setIsScreenPortraitLiveData(isPortrait)
+
+        super.onCreate(savedInstanceState)
+        setContent {
+            DualViewComposeSampleTheme {
+                SetupUI(appStateViewModel)
+            }
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        val isPortrait = newConfig.orientation == Configuration.ORIENTATION_PORTRAIT
+        appStateViewModel.setIsScreenPortraitLiveData(isPortrait)
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+
+        windowManager.registerLayoutChangeCallback(
+            mainThreadExecutor,
+            { windowLayoutInfo ->
+                appStateViewModel.setIsScreenSpannedLiveData(windowLayoutInfo.displayFeatures.size > 0)
+            }
+        )
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        windowManager.unregisterLayoutChangeCallback {}
+    }
+}
---
+++ ComposeSamples/DualView/src/main/java/com/microsoft/device/display/samples/dualview/models/AppStateViewModel.kt
@@ -0,0 +1,44 @@
+/*
+ *
+ *  * Copyright (c) Microsoft Corporation. All rights reserved.
+ *  * Licensed under the MIT License.
+ *  *
+ *
+ */
+
+package com.microsoft.device.display.samples.dualview.models
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AppStateViewModel : ViewModel() {
+
+    private val isScreenSpannedLiveData = MutableLiveData<Boolean>() // observe the screen spanning mode
+    private val selectionLiveData = MutableLiveData<Int>() // observe the image selection change
+    private val isScreenPortraitLiveData = MutableLiveData<Boolean>() // observe the screen Portrait/Landscape mode
+
+    fun getSelectionLiveData(): LiveData<Int> {
+        return this.selectionLiveData
+    }
+
+    fun setSelectionLiveData(selectedImage: Int) {
+        selectionLiveData.value = selectedImage
+    }
+
+    fun getIsScreenSpannedLiveData(): LiveData<Boolean> {
+        return this.isScreenSpannedLiveData
+    }
+
+    fun setIsScreenSpannedLiveData(isScreenSpanned: Boolean) {
+        isScreenSpannedLiveData.value = isScreenSpanned
+    }
+
+    fun getIsScreenPortraitLiveData(): LiveData<Boolean> {
+        return this.isScreenPortraitLiveData
+    }
+
+    fun setIsScreenPortraitLiveData(IsScreenPortrait: Boolean) {
+        isScreenPortraitLiveData.value = IsScreenPortrait
+    }
+}
