8e817288d1749b60a454f5c2e079bf8ff4eed684
Joy Liu <joyl1216@hotmail.com>
2021-06-17 11:03:32 -0700

    Bump Compose samples dependencies and README

--- ComposeSamples/DualView/src/main/java/com/microsoft/device/display/samples/dualview/ui/home/HomePage.kt
+++ ComposeSamples/DualView/src/main/java/com/microsoft/device/display/samples/dualview/ui/home/HomePage.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.navigate
 import androidx.navigation.compose.rememberNavController
 import com.microsoft.device.display.samples.dualview.R
 import com.microsoft.device.display.samples.dualview.models.AppStateViewModel
