704098eff4281016020813ff190708b36b3148be
Nthily <31311826+Nthily@users.noreply.github.com>
2021-06-18 23:48:57 +0200

    fix format: remove unused import

--- ComposeSamples/ListDetail/src/main/java/com/microsoft/device/display/samples/listdetail/ui/view/ListView.kt
+++ ComposeSamples/ListDetail/src/main/java/com/microsoft/device/display/samples/listdetail/ui/view/ListView.kt
@@ -5,7 +5,6 @@
 
 package com.microsoft.device.display.samples.listdetail.ui.view
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
