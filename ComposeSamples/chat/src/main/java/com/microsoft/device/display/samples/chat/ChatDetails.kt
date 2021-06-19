1b7e1570746883a929be92304cb6494e93764483
Nthily <nthily@outlook.com>
2021-06-19 13:26:27 +0200

    deleted unused import

--- ComposeSamples/chat/src/main/java/com/microsoft/device/display/samples/chat/ChatDetails.kt
+++ ComposeSamples/chat/src/main/java/com/microsoft/device/display/samples/chat/ChatDetails.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
