5e10da176b93c533ef75964079a34fa7c6bfe0c9
Nthily <nthily@outlook.com>
2021-07-03 12:10:43 +0200

    try to solve unitTest problems

---
+++ ComposeSamples/chat/src/test/java/com/microsoft/device/display/samples/chat/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.microsoft.device.display.samples.chat
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
