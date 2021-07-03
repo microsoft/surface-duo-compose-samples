be52197fd44af57377d151844d5ef0cdfbe3ad9e
Nthily <nthily@outlook.com>
2021-07-03 13:12:13 +0200

    fixed remaining formats

--- ComposeSamples/chat/src/main/java/com/microsoft/device/display/samples/chat/ChatDetails.kt
+++ ComposeSamples/chat/src/main/java/com/microsoft/device/display/samples/chat/ChatDetails.kt
@@ -1,9 +1,9 @@
 package com.microsoft.device.display.samples.chat
 
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -30,9 +30,9 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
