510a786efa75ee6a409c11867960ca6be4ec3a76
Joy Liu <joyl1216@hotmail.com>
2021-07-16 15:05:11 -0700

    Align VerticalSingle and HorizontalSingle with layout orientation

--- TwoPaneLayout/library/src/main/java/com/microsoft/device/dualscreen/twopanelayout/TwoPaneLayout.kt
+++ TwoPaneLayout/library/src/main/java/com/microsoft/device/dualscreen/twopanelayout/TwoPaneLayout.kt
@@ -87,13 +87,13 @@ fun TwoPaneLayout(
 /**
  * TwoPaneMode
  * TwoPane,          always shows two panes, regardless the orientation, by default
- * VerticalSingle    shows big single pane in vertical layout
- * HorizontalSingle  shows big single pane in horizontal layout
+ * HorizontalSingle  shows big single pane in horizontal orientation layout(top/bottom)
+ * VerticalSingle    shows big single pane in vertical orientation layout(left/right)
  */
 enum class TwoPaneMode {
     TwoPane,
-    VerticalSingle,
-    HorizontalSingle
+    HorizontalSingle,
+    VerticalSingle
 }
 
 @LayoutScopeMarker
--- TwoPaneLayout/library/src/main/java/com/microsoft/device/dualscreen/twopanelayout/screenState/ScreenState.kt
+++ TwoPaneLayout/library/src/main/java/com/microsoft/device/dualscreen/twopanelayout/screenState/ScreenState.kt
@@ -10,8 +10,8 @@ import androidx.compose.ui.geometry.Size
 
 /**
  * LayoutOrientation
- *     Horizontal,  the width of pane is bigger than the height, left/right
- *     Vertical     the height of pane is bigger than the width, top/bottom
+ *     Horizontal,  the width of hinge/folding line is bigger than the height, top/bottom
+ *     Vertical     the height of hinge/folding line is bigger than the width, left/right
  */
 enum class LayoutOrientation {
     Horizontal,
