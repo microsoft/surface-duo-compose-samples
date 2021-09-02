211bf1d9267613f0438f51c15ac950afe1afcb5b
Joy Liu <joyl1216@hotmail.com>
2021-09-02 16:42:59 -0700

    Update ListDetail with WM beta

--- ComposeSamples/ListDetail/src/main/java/com/microsoft/device/display/samples/listdetail/MainActivity.kt
+++ ComposeSamples/ListDetail/src/main/java/com/microsoft/device/display/samples/listdetail/MainActivity.kt
@@ -6,55 +6,29 @@
 package com.microsoft.device.display.samples.listdetail
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.window.DisplayFeature
-import androidx.window.WindowManager
+import androidx.window.layout.WindowInfoRepository
+import androidx.window.layout.WindowInfoRepository.Companion.windowInfoRepository
 import com.microsoft.device.display.samples.listdetail.models.AppStateViewModel
 import com.microsoft.device.display.samples.listdetail.ui.theme.ListDetailComposeSampleTheme
 import com.microsoft.device.display.samples.listdetail.ui.view.SetupUI
-import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var windowManager: WindowManager
+    private lateinit var windowInfoRep: WindowInfoRepository
     private lateinit var appStateViewModel: AppStateViewModel
 
-    private val handler = Handler(Looper.getMainLooper())
-    private val mainThreadExecutor = Executor { r: Runnable -> handler.post(r) }
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        windowManager = WindowManager(this)
+        super.onCreate(savedInstanceState)
+
+        windowInfoRep = windowInfoRepository()
         appStateViewModel = ViewModelProvider(this).get(AppStateViewModel::class.java)
 
-        super.onCreate(savedInstanceState)
         setContent {
             ListDetailComposeSampleTheme {
-                SetupUI(appStateViewModel)
+                SetupUI(appStateViewModel, windowInfoRep)
             }
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        windowManager.registerLayoutChangeCallback(
-            mainThreadExecutor,
-            { windowLayoutInfo ->
-                reserveScreenState(windowLayoutInfo.displayFeatures)
-            }
-        )
-    }
-
-    override fun onStop() {
-        super.onStop()
-        windowManager.unregisterLayoutChangeCallback {}
-    }
-
-    private fun reserveScreenState(displayFeatures: List<DisplayFeature>) {
-        val isAppSpanned = displayFeatures.isNotEmpty()
-        appStateViewModel.isAppSpannedLiveData.value = isAppSpanned
     }
 }
