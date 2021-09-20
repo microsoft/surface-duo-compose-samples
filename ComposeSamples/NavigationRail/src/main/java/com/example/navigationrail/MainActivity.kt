1c129aebf9ed69e80a5f62ad39189e18b9c3884c
Kristen Halper <khalp@vt.edu>
2021-09-20 16:28:12 -0400

    Set up basic code structure

--- ComposeSamples/NavigationRail/src/main/java/com/example/navigationrail/MainActivity.kt
+++ ComposeSamples/NavigationRail/src/main/java/com/example/navigationrail/MainActivity.kt
@@ -3,36 +3,29 @@ package com.example.navigationrail
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.window.layout.WindowInfoRepository
+import androidx.window.layout.WindowInfoRepository.Companion.windowInfoRepository
 import com.example.navigationrail.ui.theme.ComposeSamplesTheme
+import com.example.navigationrail.ui.view.SetupUI
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var windowInfoRep: WindowInfoRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        windowInfoRep = windowInfoRepository()
+
         setContent {
             ComposeSamplesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                SetupUI(windowInfoRep, window)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeSamplesTheme {
-        Greeting("Android")
     }
 }
---
+++ ComposeSamples/NavigationRail/src/main/java/com/example/navigationrail/ui/view/HomePage.kt
@@ -0,0 +1,91 @@
+package com.example.navigationrail.ui.view
+
+import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.window.layout.WindowInfoRepository
+import com.example.navigationrail.ui.theme.ComposeSamplesTheme
+import com.microsoft.device.dualscreen.twopanelayout.TwoPaneLayout
+import com.microsoft.device.dualscreen.twopanelayout.TwoPaneMode
+import kotlinx.coroutines.flow.collect
+
+const val SMALLEST_TABLET_SCREEN_WIDTH_DP = 585
+
+@Composable
+fun SetupUI(windowInfoRep: WindowInfoRepository) {
+    var isAppSpanned by remember { mutableStateOf(false) }
+
+    LaunchedEffect(windowInfoRep) {
+        windowInfoRep.windowLayoutInfo
+            .collect { newLayoutInfo ->
+                val displayFeatures = newLayoutInfo.displayFeatures
+                isAppSpanned = displayFeatures.isNotEmpty()
+            }
+    }
+
+    val isPortrait = LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT
+    val smallestScreenWidthDp = LocalConfiguration.current.smallestScreenWidthDp
+    val isTablet = smallestScreenWidthDp > SMALLEST_TABLET_SCREEN_WIDTH_DP
+    val isDualScreen = (isAppSpanned || isTablet) && !isPortrait
+
+    DualScreenUI(isDualScreen, isPortrait)
+}
+
+@Composable
+fun DualScreenUI(isDualScreen: Boolean, isPortrait: Boolean) {
+    TwoPaneLayout(
+        paneMode = TwoPaneMode.HorizontalSingle,
+        pane1 = { Pane1(isDualScreen, isPortrait) },
+        pane2 = { Pane2() },
+    )
+}
+
+@Composable
+fun Pane1(isDualScreen: Boolean, isPortrait: Boolean) {
+}
+
+@Composable
+fun Pane2() {
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewPane1PortraitDarkMode() {
+    ComposeSamplesTheme {
+        Pane1(isDualScreen = false, isPortrait = false)
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
+@Composable
+fun PreviewPane1PortraitLightMode() {
+    ComposeSamplesTheme {
+        Pane1(isDualScreen = false, isPortrait = false)
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewPane2DarkMode() {
+    ComposeSamplesTheme {
+        Pane2()
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
+@Composable
+fun PreviewPane2LightMode() {
+    ComposeSamplesTheme {
+        Pane2()
+    }
+}
