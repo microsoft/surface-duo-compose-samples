b89a9afdb3b603ceaeaa8f74b5a948dd8b483372
Kristen Halper <khalp@vt.edu>
2021-10-01 15:58:32 -0400

    Add roboto font family

--- ComposeSamples/NavigationRail/src/main/java/com/microsoft/device/display/samples/navigationrail/ui/theme/Type.kt
+++ ComposeSamples/NavigationRail/src/main/java/com/microsoft/device/display/samples/navigationrail/ui/theme/Type.kt
@@ -33,6 +33,21 @@ val montserratFontFamily = FontFamily(
     Font(R.font.montserrat_thin_italic, FontWeight.Thin, FontStyle.Italic),
 )
 
+val robotoFontFamily = FontFamily(
+    Font(R.font.roboto_black, FontWeight.Black),
+    Font(R.font.roboto_black_italic, FontWeight.Black, FontStyle.Italic),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+    Font(R.font.roboto_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.roboto_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_thin, FontWeight.Thin),
+    Font(R.font.roboto_thin_italic, FontWeight.Thin, FontStyle.Italic),
+)
+
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
