66dd8a44b445a48dc915b45b0e1f75f0f00bdb9a
Kristen Halper <khalp@vt.edu>
2021-10-04 18:24:45 -0400

    Add nature images

--- ComposeSamples/NavigationRail/src/main/java/com/microsoft/device/display/samples/navigationrail/models/DataProvider.kt
+++ ComposeSamples/NavigationRail/src/main/java/com/microsoft/device/display/samples/navigationrail/models/DataProvider.kt
@@ -11,7 +11,7 @@ object DataProvider {
     private val images: List<Image> = listOf(
         Image(
             id = 1,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.plant_1,
             description = "Description",
             date = "9/23",
             details = "discovery details\ndiscovery details\n" +
@@ -32,179 +32,424 @@ object DataProvider {
         ),
         Image(
             id = 2,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.plant_2,
             description = "Description",
             date = "6/26",
             details = "discovery details"
         ),
         Image(
             id = 3,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.plant_3,
             description = "Description",
             date = "4/24",
             details = "discovery details"
         ),
         Image(
             id = 4,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.plant_4,
             description = "Description",
             date = "3/11",
             details = "discovery details"
         ),
         Image(
             id = 5,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.plant_5,
             description = "Description",
             date = "6/12",
             details = "discovery details"
         ),
         Image(
             id = 6,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.plant_6,
             description = "Description",
             date = "9/23",
             details = "discovery details"
         ),
         Image(
             id = 7,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.plant_7,
             description = "Description",
             date = "6/26",
             details = "discovery details"
         ),
         Image(
             id = 8,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.plant_8,
             description = "Description",
             date = "4/24",
             details = "discovery details"
         ),
         Image(
             id = 9,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.plant_9,
             description = "Description",
             date = "3/11",
             details = "discovery details"
         ),
         Image(
             id = 10,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.plant_10,
             description = "Description",
             date = "6/12",
             details = "discovery details"
         ),
         Image(
             id = 11,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.plant_11,
             description = "Description",
             date = "9/23",
             details = "discovery details"
         ),
         Image(
             id = 12,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.plant_12,
             description = "Description",
             date = "6/26",
             details = "discovery details"
         ),
         Image(
             id = 13,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.bird_1,
             description = "Description",
             date = "4/24",
             details = "discovery details"
         ),
         Image(
             id = 14,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.bird_2,
             description = "Description",
             date = "3/11",
             details = "discovery details"
         ),
         Image(
             id = 15,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.bird_3,
             description = "Description",
             date = "6/12",
             details = "discovery details"
         ),
         Image(
             id = 16,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.bird_4,
             description = "Description",
             date = "4/24",
             details = "discovery details"
         ),
         Image(
             id = 17,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.bird_5,
             description = "Description",
             date = "4/24",
             details = "discovery details"
         ),
         Image(
             id = 18,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.bird_6,
             description = "Description",
             date = "4/24",
             details = "discovery details"
         ),
         Image(
             id = 19,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.bird_7,
             description = "Description",
             date = "3/11",
             details = "discovery details"
         ),
         Image(
             id = 20,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.bird_8,
             description = "Description",
             date = "6/12",
             details = "discovery details"
         ),
         Image(
             id = 21,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.bird_9,
             description = "Description",
             date = "4/24",
             details = "discovery details"
         ),
         Image(
             id = 22,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.bird_10,
             description = "Description",
             date = "4/24",
             details = "discovery details"
         ),
         Image(
             id = 23,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.bird_11,
             description = "Description",
             date = "4/24",
             details = "discovery details"
         ),
         Image(
             id = 24,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.bird_12,
             description = "Description",
             date = "4/24",
             details = "discovery details"
         ),
         Image(
             id = 25,
-            image = R.drawable.ic_launcher_foreground,
+            image = R.drawable.animal_1,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 26,
+            image = R.drawable.animal_2,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 27,
+            image = R.drawable.animal_3,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 28,
+            image = R.drawable.animal_4,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 29,
+            image = R.drawable.animal_5,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 30,
+            image = R.drawable.animal_6,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 31,
+            image = R.drawable.animal_7,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 32,
+            image = R.drawable.animal_8,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 33,
+            image = R.drawable.animal_9,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 34,
+            image = R.drawable.animal_10,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 35,
+            image = R.drawable.animal_11,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 36,
+            image = R.drawable.animal_12,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 37,
+            image = R.drawable.lake_1,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 38,
+            image = R.drawable.lake_2,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 39,
+            image = R.drawable.lake_3,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 40,
+            image = R.drawable.lake_4,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 41,
+            image = R.drawable.lake_5,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 42,
+            image = R.drawable.lake_6,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 43,
+            image = R.drawable.lake_7,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 44,
+            image = R.drawable.lake_8,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 45,
+            image = R.drawable.lake_9,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 46,
+            image = R.drawable.lake_10,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 47,
+            image = R.drawable.lake_11,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 48,
+            image = R.drawable.lake_12,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 49,
+            image = R.drawable.rock_1,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 50,
+            image = R.drawable.rock_2,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 51,
+            image = R.drawable.rock_3,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 52,
+            image = R.drawable.rock_4,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 53,
+            image = R.drawable.rock_5,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 54,
+            image = R.drawable.rock_6,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 55,
+            image = R.drawable.rock_7,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 56,
+            image = R.drawable.rock_8,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 57,
+            image = R.drawable.rock_9,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 58,
+            image = R.drawable.rock_10,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 59,
+            image = R.drawable.rock_11,
+            description = "Description",
+            date = "4/24",
+            details = "discovery details"
+        ),
+        Image(
+            id = 60,
+            image = R.drawable.rock_12,
             description = "Description",
             date = "4/24",
             details = "discovery details"
         ),
     )
 
-    val plantList = images.subList(0, 15)
-    val birdList = images.subList(15, 17)
-    val animalList = images.subList(17, 20)
-    val rockList = images.subList(20, 21)
-    val lakeList = images.subList(21, 25)
+    val plantList = images.subList(0, 12)
+    val birdList = images.subList(12, 24)
+    val animalList = images.subList(24, 36)
+    val lakeList = images.subList(36, 48)
+    val rockList = images.subList(48, 60)
 
     fun getImage(imageId: Int) = images.find { it.id == imageId }
 }
