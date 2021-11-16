739b9a7ca8709d9800e0ce1dd4f30a318777e6ca
Kristen Halper <khalp@vt.edu>
2021-11-16 17:40:18 -0500

    Split up home page file

--- CompanionPane/src/main/java/com/microsoft/device/display/samples/companionpane/HomePage.kt
+++ CompanionPane/src/main/java/com/microsoft/device/display/samples/companionpane/HomePage.kt
@@ -5,21 +5,7 @@
 
 package com.microsoft.device.display.samples.companionpane
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import android.content.res.Configuration
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -29,26 +15,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.microsoft.device.display.samples.companionpane.ui.components.BrightnessPanel
-import com.microsoft.device.display.samples.companionpane.ui.components.DefinitionPanel
-import com.microsoft.device.display.samples.companionpane.ui.components.EffectPanel
-import com.microsoft.device.display.samples.companionpane.ui.components.FullFilterControl
-import com.microsoft.device.display.samples.companionpane.ui.components.ImagePanel
-import com.microsoft.device.display.samples.companionpane.ui.components.MagicWandPanel
-import com.microsoft.device.display.samples.companionpane.ui.components.ShortFilterControl
-import com.microsoft.device.display.samples.companionpane.ui.components.VignettePanel
+import androidx.window.layout.FoldingFeature
+import androidx.window.layout.WindowLayoutInfo
+import com.microsoft.device.display.samples.companionpane.ui.view.DualLandscapePane1
+import com.microsoft.device.display.samples.companionpane.ui.view.DualLandscapePane2
+import com.microsoft.device.display.samples.companionpane.ui.view.DualPortraitPane1
+import com.microsoft.device.display.samples.companionpane.ui.view.DualPortraitPane2
+import com.microsoft.device.display.samples.companionpane.ui.view.LandscapeLayout
+import com.microsoft.device.display.samples.companionpane.ui.view.PortraitLayout
 import com.microsoft.device.dualscreen.twopanelayout.TwoPaneLayout
 import com.microsoft.device.dualscreen.windowstate.WindowMode
 import com.microsoft.device.dualscreen.windowstate.WindowState
 
-private val shortSlideWidth = 200.dp
-private val longSlideWidth = 350.dp
 
 @Composable
 fun CompanionPaneApp(windowState: WindowState) {
@@ -65,7 +45,7 @@ fun CompanionPaneApp(windowState: WindowState) {
 fun CompanionPaneTopBar(title: String? = null) {
     TopAppBar(
         title = { Text(text = title ?: "") },
-        backgroundColor = MaterialTheme.colors.primaryVariant
+        backgroundColor = MaterialTheme.colors.primary
     )
 }
 
@@ -95,129 +75,5 @@ fun Pane2(windowMode: WindowMode) {
         }
         WindowMode.DUAL_LANDSCAPE -> DualLandscapePane2()
         else -> {}
-    }
-}
-
-@Composable
-fun DualPortraitPane1() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceEvenly
-    ) {
-        ImagePanel(Modifier.padding(horizontal = 30.dp))
-        EffectPanel()
-    }
-}
-
-@Composable
-fun DualPortraitPane2() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(80.dp, Alignment.CenterVertically)
-    ) {
-        Column(verticalArrangement = Arrangement.spacedBy(35.dp)) {
-            MagicWandPanel(modifier = Modifier.width(longSlideWidth))
-            DefinitionPanel(modifier = Modifier.width(longSlideWidth))
-            VignettePanel(modifier = Modifier.width(longSlideWidth))
-            BrightnessPanel(modifier = Modifier.width(longSlideWidth))
-        }
-        ShortFilterControl()
-    }
-}
-
-@Composable
-fun DualLandscapePane1() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(bottom = 20.dp)
-            .clipToBounds(),
-        contentAlignment = Alignment.Center
-    ) {
-        ImagePanel(modifier = Modifier.padding(all = 20.dp))
-    }
-}
-
-@Composable
-fun DualLandscapePane2() {
-    Column(
-        modifier = Modifier
-            .background(color = MaterialTheme.colors.background)
-            .fillMaxSize()
-            .padding(start = 5.dp, end = 5.dp, bottom = 5.dp),
-        verticalArrangement = Arrangement.SpaceEvenly,
-    ) {
-        EffectPanel()
-        Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier
-                .fillMaxWidth()
-                .horizontalScroll(
-                    rememberScrollState()
-                )
-        ) {
-            Column(verticalArrangement = Arrangement.SpaceEvenly) {
-                MagicWandPanel(modifier = Modifier.width(shortSlideWidth))
-                Spacer(Modifier.height(20.dp))
-                DefinitionPanel(modifier = Modifier.width(shortSlideWidth))
-            }
-            Column(verticalArrangement = Arrangement.SpaceEvenly) {
-                VignettePanel(modifier = Modifier.width(shortSlideWidth))
-                Spacer(Modifier.height(20.dp))
-                BrightnessPanel(modifier = Modifier.width(shortSlideWidth))
-            }
-        }
-        ShortFilterControl()
-    }
-}
-
-@Composable
-fun PortraitLayout() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(15.dp)
-    ) {
-        Spacer(Modifier.height(8.dp))
-        ImagePanel(
-            Modifier
-                .padding(start = 50.dp, end = 50.dp)
-                .wrapContentHeight()
-        )
-        EffectPanel()
-        FullFilterControl()
-    }
-}
-
-@Composable
-fun LandscapeLayout() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 20.dp, start = 20.dp, end = 10.dp, bottom = 10.dp)
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.SpaceAround
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(bottom = 10.dp),
-            horizontalArrangement = Arrangement.spacedBy(20.dp)
-        ) {
-            ImagePanel(Modifier.weight(1f))
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .weight(1f)
-                    .padding(top = 10.dp),
-                verticalArrangement = Arrangement.spacedBy(20.dp)
-            ) {
-                MagicWandPanel(modifier = Modifier.width(shortSlideWidth))
-                DefinitionPanel(modifier = Modifier.width(shortSlideWidth))
-                VignettePanel(modifier = Modifier.width(shortSlideWidth))
-                BrightnessPanel(modifier = Modifier.width(shortSlideWidth))
-            }
-        }
-        ShortFilterControl()
     }
 }
