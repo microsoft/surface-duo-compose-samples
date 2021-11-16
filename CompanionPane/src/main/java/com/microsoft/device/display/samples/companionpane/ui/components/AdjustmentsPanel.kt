905a535d1e3011b74a6d8c3fb23bd9cc9b252d8d
Kristen Halper <khalp@vt.edu>
2021-11-16 17:41:52 -0500

    Clean up effects panel code

--- CompanionPane/src/main/java/com/microsoft/device/display/samples/companionpane/ui/components/AdjustmentsPanel.kt
+++ CompanionPane/src/main/java/com/microsoft/device/display/samples/companionpane/ui/components/AdjustmentsPanel.kt
@@ -5,43 +5,42 @@
 
 package com.microsoft.device.display.samples.companionpane.ui.components
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.microsoft.device.display.samples.companionpane.R
 
-val iconWidth = 25.dp
-val controlWidth = 70.dp
+private val iconWidth = 25.dp
+private val controlWidth = 70.dp
 
 @Composable
 fun MagicWandPanel(modifier: Modifier) {
-    Row() {
-        ImageWithText(R.drawable.filter_icon, "Magic Wand", iconWidth, controlWidth)
-        SliderControl(modifier)
-    }
+    AdjustmentPanel(modifier, R.drawable.filter_icon, R.string.magic_wand)
 }
 
 @Composable
 fun DefinitionPanel(modifier: Modifier) {
-    Row() {
-        ImageWithText(R.drawable.hdr_icon, "Definition", iconWidth, controlWidth)
-        SliderControl(modifier)
-    }
+    AdjustmentPanel(modifier, R.drawable.hdr_icon, R.string.definition)
 }
 
 @Composable
 fun VignettePanel(modifier: Modifier) {
-    Row() {
-        ImageWithText(R.drawable.zoom_icon, "Vignette", iconWidth, controlWidth)
-        SliderControl(modifier)
-    }
+    AdjustmentPanel(modifier, R.drawable.zoom_icon, R.string.vignette)
 }
 
 @Composable
 fun BrightnessPanel(modifier: Modifier) {
-    Row() {
-        ImageWithText(R.drawable.brightness_icon, "Brightness", iconWidth, controlWidth)
+    AdjustmentPanel(modifier, R.drawable.brightness_icon, R.string.brightness)
+}
+
+@Composable
+private fun AdjustmentPanel(modifier: Modifier, @DrawableRes icon: Int, @StringRes label: Int) {
+    Row {
+        ImageWithText(icon, stringResource(label), iconWidth, controlWidth)
         SliderControl(modifier)
     }
 }
--- CompanionPane/src/main/java/com/microsoft/device/display/samples/companionpane/ui/components/EffectPanel.kt
+++ CompanionPane/src/main/java/com/microsoft/device/display/samples/companionpane/ui/components/EffectPanel.kt
@@ -5,51 +5,84 @@
 
 package com.microsoft.device.display.samples.companionpane.ui.components
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.microsoft.device.display.samples.companionpane.R
 
+private val imageWidth = 80.dp
+private val controlWidth = 100.dp
+
+enum class Filters(@StringRes val title: Int, @DrawableRes val image: Int) {
+    GINGHAM(R.string.gingham, R.drawable.gingham),
+    ORIGINAL(R.string.original, R.drawable.original),
+    LARK(R.string.lark, R.drawable.lark),
+    JUNO(R.string.juno, R.drawable.juno),
+    LUDWIG(R.string.ludwig, R.drawable.ludwig)
+}
+
+private val filterList = Filters.values()
+
+private val fullIconList = listOf<@DrawableRes Int>(
+    R.drawable.filter_icon,
+    R.drawable.hdr_icon,
+    R.drawable.ecllipse_icon,
+    R.drawable.vertical_icon,
+    R.drawable.horizontal_icon,
+    R.drawable.zoom_icon,
+    R.drawable.brightness_icon
+)
+
+private val shortIconList = fullIconList.subList(2, 5)
+
 @Composable
-fun EffectPanel() {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        val imageWidth = 80.dp
-        val controlWidth = 100.dp
-        LeftAlignText(title = "Filters")
-        Spacer(Modifier.height(10.dp))
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-            Box(modifier = Modifier.weight(1f).clipToBounds()) {
-                ImageWithText(R.drawable.gingham, "Gingham", imageWidth, controlWidth)
-            }
-            Box(modifier = Modifier.weight(1f).clipToBounds()) {
-                ImageWithText(R.drawable.orignal, "Original", imageWidth, controlWidth)
-            }
-            Box(modifier = Modifier.weight(1f).clipToBounds()) {
-                ImageWithText(R.drawable.orignal, "Lark", imageWidth, controlWidth)
-            }
-            Box(modifier = Modifier.weight(1f).clipToBounds()) {
-                ImageWithText(R.drawable.orignal, "Juno", imageWidth, controlWidth)
-            }
-            Box(modifier = Modifier.weight(1f).clipToBounds()) {
-                ImageWithText(R.drawable.orignal, "Ludwig", imageWidth, controlWidth)
+fun FilterPanel() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.Bottom,
+    ) {
+        filterList.forEachIndexed { index, filter ->
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                if (index == 0) {
+                    // Show filters label over first filter option
+                    Text(
+                        text = stringResource(R.string.filters),
+                        color = MaterialTheme.colors.onSurface,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp,
+                    )
+                }
+                ImageWithText(filter.image, stringResource(filter.title), imageWidth, controlWidth)
             }
         }
     }
 }
+
 
 @Composable
 fun ShortFilterControl() {
@@ -115,34 +148,12 @@ fun FullIconsPanel() {
             .height(25.dp),
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.filter_icon),
-            contentDescription = null
-        )
-        Image(
-            painter = painterResource(id = R.drawable.hdr_icon),
-            contentDescription = null
-        )
-        Image(
-            painter = painterResource(id = R.drawable.ecllipse_icon),
-            contentDescription = null
-        )
-        Image(
-            painter = painterResource(id = R.drawable.vertical_icon),
-            contentDescription = null
-        )
-        Image(
-            painter = painterResource(id = R.drawable.horizontal_icon),
-            contentDescription = null
-        )
-        Image(
-            painter = painterResource(id = R.drawable.zoom_icon),
-            contentDescription = null
-        )
-        Image(
-            painter = painterResource(id = R.drawable.brightness_icon),
-            contentDescription = null
-        )
+        fullIconList.forEach { icon ->
+            Image(
+                painter = painterResource(id = icon),
+                contentDescription = null,
+            )
+        }
     }
 }
 
@@ -154,17 +165,11 @@ fun ShortIconsPanel() {
             .height(25.dp),
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.ecllipse_icon),
-            contentDescription = null
-        )
-        Image(
-            painter = painterResource(id = R.drawable.vertical_icon),
-            contentDescription = null
-        )
-        Image(
-            painter = painterResource(id = R.drawable.horizontal_icon),
-            contentDescription = null
-        )
+        shortIconList.forEach { icon ->
+            Image(
+                painter = painterResource(id = icon),
+                contentDescription = null,
+            )
+        }
     }
 }
