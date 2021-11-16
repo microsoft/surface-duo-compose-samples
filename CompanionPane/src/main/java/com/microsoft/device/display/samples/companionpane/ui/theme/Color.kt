8028208d32940b337286109959a62bcf368126fa
Kristen Halper <khalp@vt.edu>
2021-11-16 17:39:36 -0500

    Clean up theming

--- CompanionPane/src/main/java/com/microsoft/device/display/samples/companionpane/ui/theme/Color.kt
+++ CompanionPane/src/main/java/com/microsoft/device/display/samples/companionpane/ui/theme/Color.kt
@@ -7,10 +7,6 @@ package com.microsoft.device.display.samples.companionpane.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val purple200 = Color(0xFFBB86FC)
-val purple500 = Color(0xFF6200EE)
-val purple700 = Color(0xFF3700B3)
-val teal200 = Color(0xFF03DAC5)
 val darkGray = Color(0xFF2B2B2B)
-val Gray = Color(0xFF555555)
-val blueTint = Color(0xFF2BA1FF)
+val gray = Color(0xFF555555)
+val lightGray = Color.Gray
--- CompanionPane/src/main/java/com/microsoft/device/display/samples/companionpane/ui/theme/Theme.kt
+++ CompanionPane/src/main/java/com/microsoft/device/display/samples/companionpane/ui/theme/Theme.kt
@@ -8,41 +8,28 @@ package com.microsoft.device.display.samples.companionpane.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Color.White,
-    primaryVariant = Color.Gray,
+    primary = lightGray,
+    primaryVariant = gray,
     onPrimary = darkGray,
-    secondary = darkGray,
-    background = darkGray
-)
-
-private val LightColorPalette = lightColors(
-    primary = Color.White,
-    primaryVariant = Color.Gray,
-    onPrimary = darkGray,
-    secondary = darkGray,
+    secondary = Color.White,
     background = darkGray,
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    surface = darkGray,
 )
 
 @Composable
-fun CompanionPaneAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun CompanionPaneAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
-        LightColorPalette
+        // Using one palette for now, can change this in the future
+        DarkColorPalette
     }
 
     MaterialTheme(
