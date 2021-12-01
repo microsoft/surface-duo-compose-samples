7ceadc96d62e7acda7385d98d05d0772a18e22de
Kristen Halper <khalp@vt.edu>
2021-12-01 15:52:15 -0500

    Update two page to use module

--- TwoPage/src/main/java/com/microsoft/device/display/samples/twopage/MainActivity.kt
+++ TwoPage/src/main/java/com/microsoft/device/display/samples/twopage/MainActivity.kt
@@ -8,21 +8,22 @@ package com.microsoft.device.display.samples.twopage
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.window.layout.WindowInfoTracker
-import com.microsoft.device.display.samples.twopage.ui.home.SetupUI
+import com.microsoft.device.display.samples.twopage.ui.home.TwoPageApp
 import com.microsoft.device.display.samples.twopage.ui.theme.TwoPageComposeSamplesTheme
+import com.microsoft.device.dualscreen.window.WindowState
+import com.microsoft.device.dualscreen.window.rememberWindowState
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var windowInfoRep: WindowInfoTracker
+    private lateinit var windowState: WindowState
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        windowInfoRep = WindowInfoTracker.getOrCreate(this)
-
         setContent {
+            windowState = rememberWindowState()
+
             TwoPageComposeSamplesTheme {
-                SetupUI(windowInfoRep)
+                TwoPageApp(windowState)
             }
         }
     }
