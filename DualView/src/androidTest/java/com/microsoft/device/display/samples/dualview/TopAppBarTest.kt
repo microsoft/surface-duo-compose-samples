74d0ea3870fd3be5f268d7f0ed107eee0ca881da
Kristen Halper <khalp@vt.edu>
2021-12-21 17:34:20 -0500

    Add test dependencies and blank test class

---
+++ DualView/src/androidTest/java/com/microsoft/device/display/samples/dualview/TopAppBarTest.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+package com.microsoft.device.display.samples.dualview
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import androidx.window.testing.layout.WindowLayoutInfoPublisherRule
+import org.junit.Rule
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+
+class TopAppBarTest {
+    private val composeTestRule = createAndroidComposeRule<MainActivity>()
+    private val publisherRule = WindowLayoutInfoPublisherRule()
+
+    @get: Rule
+    val testRule: TestRule
+
+    init {
+        testRule = RuleChain.outerRule(publisherRule).around(composeTestRule)
+        RuleChain.outerRule(composeTestRule)
+    }
+}
