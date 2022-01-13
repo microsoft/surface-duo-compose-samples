9a256cdff5efc00cf15e89fda21b28bdd23aede5
Kristen Halper <khalp@vt.edu>
2022-01-13 17:22:10 -0500

    Add string helper method that accepts formatting arguments

--- TestUtils/src/main/java/com/microsoft/device/dualscreen/testutils/StringHelper.kt
+++ TestUtils/src/main/java/com/microsoft/device/dualscreen/testutils/StringHelper.kt
@@ -9,6 +9,7 @@ import androidx.activity.ComponentActivity
 import androidx.annotation.StringRes
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import java.util.Objects
 
 /**
  * STRING HELPER
@@ -23,4 +24,14 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
  */
 fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.getString(@StringRes id: Int): String {
     return activity.getString(id)
+}
+
+/**
+ * Get resource string inside Compose test with resource id and arguments
+ *
+ * @param id: string resource id
+ * @param formatArgs: arguments to string
+ */
+fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.getString(@StringRes id: Int, vararg formatArgs: Any): String {
+    return activity.getString(id, *formatArgs)
 }
