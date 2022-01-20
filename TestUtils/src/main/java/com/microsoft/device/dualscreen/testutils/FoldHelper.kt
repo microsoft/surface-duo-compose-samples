d5126b71c761d26ab2f25c455be556c975bbb280
Kristen Halper <khalp@vt.edu>
2022-01-20 15:00:53 -0500

    Remove unfold method until we can test it

--- TestUtils/src/main/java/com/microsoft/device/dualscreen/testutils/FoldHelper.kt
+++ TestUtils/src/main/java/com/microsoft/device/dualscreen/testutils/FoldHelper.kt
@@ -84,15 +84,6 @@ fun <A : ComponentActivity> WindowLayoutInfoPublisherRule.simulateFold(
 }
 
 /**
- * Simulate the absence of a fold
- *
- * @param activityRule: test activity rule
- */
-fun <A : ComponentActivity> WindowLayoutInfoPublisherRule.unfold(activityRule: ActivityScenarioRule<A>) {
-    activityRule.scenario.onActivity { overrideWindowLayoutInfo(TestWindowLayoutInfo(emptyList())) }
-}
-
-/**
  * Simulate a vertical fold in a Compose test
  *
  * @param composeTestRule: Compose android test rule
@@ -143,15 +134,4 @@ fun <A : ComponentActivity> WindowLayoutInfoPublisherRule.simulateFold(
     orientation: FoldingFeature.Orientation,
 ) {
     simulateFold(composeTestRule.activityRule, center, size, state, orientation)
-}
-
-/**
- * Simulate the absence of a fold
- *
- * @param composeTestRule: Compose android test rule
- */
-fun <A : ComponentActivity> WindowLayoutInfoPublisherRule.unfold(
-    composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<A>, A>
-) {
-    unfold(composeTestRule.activityRule)
 }
