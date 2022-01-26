5b81b254063a86e5ff76f84ae84a14364f464f49
Joy Liu <joyliu1@microsoft.com>
2022-01-25 16:35:27 -0800

    Remove the size restriction when comparing the image in TestUtils

--- TestUtils/src/main/java/com/microsoft/device/dualscreen/testutils/ScreenshotComparator.kt
+++ TestUtils/src/main/java/com/microsoft/device/dualscreen/testutils/ScreenshotComparator.kt
@@ -82,10 +82,6 @@ fun saveScreenshotToDevice(filename: String, node: SemanticsNodeInteraction) {
  */
 @RequiresApi(Build.VERSION_CODES.O)
 fun Bitmap.compare(other: Bitmap): Boolean {
-    if (this.width != other.width || this.height != other.height) {
-        throw AssertionError("Size of screenshot does not match golden file. Expected: $width $height Actual: ${other.width} ${other.height}")
-    }
-
     // Compare row by row to save memory on device
     val row1 = IntArray(width)
     val row2 = IntArray(width)
