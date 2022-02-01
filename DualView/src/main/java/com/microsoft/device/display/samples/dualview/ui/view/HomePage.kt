fb114aaa88fd9122cb7e73a7dd74b2e598fe717b
Kristen Halper <khalp@vt.edu>
2022-02-01 18:23:33 -0500

    Update foldablePaneWidth usage in DualView sample

--- DualView/src/main/java/com/microsoft/device/display/samples/dualview/ui/view/HomePage.kt
+++ DualView/src/main/java/com/microsoft/device/display/samples/dualview/ui/view/HomePage.kt
@@ -10,20 +10,27 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import com.microsoft.device.dualscreen.twopanelayout.TwoPaneLayout
 import com.microsoft.device.dualscreen.windowstate.WindowState
+import kotlin.math.roundToInt
 
 @Composable
 fun DualViewApp(windowState: WindowState, viewSize: Int? = null) {
     var selectedIndex by rememberSaveable { mutableStateOf(-1) }
     val updateSelectedIndex: (Int) -> Unit = { newIndex -> selectedIndex = newIndex }
+    val pane1SizeWidthDp = windowState.pane1SizeDp().width.dp
+    val screenWidthDp = LocalConfiguration.current.screenWidthDp.dp
+    val viewWidth = if (windowState.isDualScreen()) pane1SizeWidthDp else screenWidthDp
 
     DualViewAppContent(
-        windowState.isDualScreen(),
-        windowState.foldablePaneWidth,
-        selectedIndex,
-        updateSelectedIndex,
-        viewSize
+        isDualScreen = windowState.isDualScreen(),
+        viewWidth = with(LocalDensity.current) { viewWidth.toPx() }.roundToInt(),
+        selectedIndex = selectedIndex,
+        updateSelectedIndex = updateSelectedIndex,
+        viewSize = viewSize
     )
 }
 
