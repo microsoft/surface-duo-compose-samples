1752246b741cd01d2f28be49f06b25729d1581c9
Levi Ortega <leviortega1234@gmail.com>
2022-06-09 13:40:49 -0600

    Added top bar styling

--- Calculator/src/main/java/com/microsoft/device/display/samples/calculator/ui/theme/Theme.kt
+++ Calculator/src/main/java/com/microsoft/device/display/samples/calculator/ui/theme/Theme.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Blue200,
@@ -35,11 +36,15 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun CalculatorTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+
+    val systemUiController = rememberSystemUiController()
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
     }
+
+    systemUiController.setSystemBarsColor(color = colors.background)
 
     MaterialTheme(
         colors = colors,
