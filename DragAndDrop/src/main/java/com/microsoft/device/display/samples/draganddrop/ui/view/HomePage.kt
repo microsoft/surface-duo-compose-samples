5d3ed5ab8097f309ec70550dc2aa7456aaf6c3d0
Joy Liu <joyliu1@microsoft.com>
2022-06-16 11:47:10 -0700

    Update

--- DragAndDrop/src/main/java/com/microsoft/device/display/samples/draganddrop/ui/view/HomePage.kt
+++ DragAndDrop/src/main/java/com/microsoft/device/display/samples/draganddrop/ui/view/HomePage.kt
@@ -27,7 +27,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
