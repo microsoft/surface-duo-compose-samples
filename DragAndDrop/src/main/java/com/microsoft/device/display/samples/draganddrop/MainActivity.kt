a49a3fc471e1790a7e5bf23cacc1ed089179d8c5
Joy Liu <joyliu1@microsoft.com>
2022-06-21 18:02:59 -0700

    Introduce DragAndDrop sample (#88)
    * Create DragAndDrop sample
    
    * Add DragTarget and DropTarget
    
    * Building UI
    
    * Update DragContainer and DropContainer
    
    * Implement text drag and drop
    
    * Introduce DragData and MimiType
    
    * Update system bar color
    
    * Fix the draggable size
    
    * Build UI in the single-pane mode
    
    * Add floating button to reset the UI
    
    * Add highlight interaction when dragging
    
    * Update dependencies
    
    * Add UI test case
    
    * Address comments
    
    * Re-introduce the project
    
    * Update README
    
    * Code clean-up
    
    * Update screenshot to gif
    
    * Update app icon
    
    * Remove DS_Store and log

---
+++ DragAndDrop/src/main/java/com/microsoft/device/display/samples/draganddrop/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.microsoft.device.display.samples.draganddrop
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.microsoft.device.display.samples.draganddrop.ui.theme.DragAndDropSamplesTheme
+import com.microsoft.device.display.samples.draganddrop.ui.view.DragAndDropApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DragAndDropSamplesTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.surface) {
+                    DragAndDropApp()
+                }
+            }
+        }
+    }
+}
