b8361635fbfe8bb59031b0558cbb759726036b94
Unmesh Padhye <unmeshpadhye@gmail.com>
2022-07-04 14:44:37 -0700

    Copyright content added to Source Editor module

--- SourceEditorCompose/src/main/java/com/microsoft/device/display/samples/sourceeditorcompose/ui/theme/Theme.kt
+++ SourceEditorCompose/src/main/java/com/microsoft/device/display/samples/sourceeditorcompose/ui/theme/Theme.kt
@@ -1,3 +1,10 @@
+/*
+ *
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ *
+ */
+
 package com.microsoft.device.display.samples.sourceeditorcompose.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
