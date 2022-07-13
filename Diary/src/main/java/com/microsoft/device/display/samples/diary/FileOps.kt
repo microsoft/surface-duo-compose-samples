a2786aa8063922b957fda112c7d04c8ae3b8929c
Unmesh Padhye <unmeshpadhye@gmail.com>
2022-07-13 00:24:14 -0700

    Hardcoded string updated to use stringResource

--- Diary/src/main/java/com/microsoft/device/display/samples/diary/FileOps.kt
+++ Diary/src/main/java/com/microsoft/device/display/samples/diary/FileOps.kt
@@ -1,6 +1,12 @@
 package com.microsoft.device.display.samples.diary
 
+
+import android.content.ContentResolver
+import android.content.Context
+import android.provider.Settings.Global.getString
 import android.util.Log
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
@@ -9,9 +15,9 @@ import java.io.IOException
 
 fun readDayFile(fileName: String, rootDir: File): String {
     val file = File(rootDir, "/$fileName")
-
+    val noContent: String = R.string.no_diary_content.toString()
     if (!file.exists()) {
-        return "No data"
+        return noContent
     }
     val fileReader = FileReader(file)
 
